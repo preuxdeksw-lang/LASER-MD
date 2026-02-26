00728e5a07eb59d74aa0716667fe3dc825479b63
TECHGOAT333 <techgoat681@gmail.com>
2026-02-26 09:57:06 -0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,7 +10,7 @@ const settings = {
   storeWriteInterval: 10000,
   description: "This is a bot for managing group commands and automating tasks.",
   version: "3.0.7",
-  updateZipUrl: "add your name github archiver",
+  updateZipUrl: "https://github.com/TECHGOAT333/LASER-MD/archive/refs/heads/main.zip'",
 };
 
 module.exports = settings;
