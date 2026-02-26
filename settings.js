92f20f88e95758e81dbe0ae966d707201983e1de
TECHGOAT333 <techgoat681@gmail.com>
2026-02-25 23:25:10 -0500

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: '𝐋𝐀𝐒𝐄𝐑 𝐌𝐃',
+  author: '‎',
+  botName: "𝐋𝐀𝐒𝐄𝐑 𝐌𝐃",
+  botOwner: '𝙳𝙴𝚅𝚆𝙴𝙴𝙳', // Your name
+  ownerNumber: '50939032060', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "3.0.7",
+  updateZipUrl: "add your name github archiver",
+};
+
+module.exports = settings;
