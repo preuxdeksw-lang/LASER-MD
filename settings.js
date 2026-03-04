163241e847adc0800d6c8e437bdc83875cf015a0
TECHGOAT333 <techgoat681@gmail.com>
2026-03-04 11:06:23 -0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -9,7 +9,7 @@ const settings = {
   maxStoreMessages: 20, 
   storeWriteInterval: 10000,
   description: "This is a bot for managing group commands and automating tasks.",
-  version: "3.0.7",
+  version: "3.0.9",
   updateZipUrl: "https://github.com/TECHGOAT333/LASER-MD/archive/refs/heads/main.zip'",
 };
 
