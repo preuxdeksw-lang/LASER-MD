0d21a902b654b1ccf8ce240de7405252b2aa95fd
TECHGOAT333 <techgoat681@gmail.com>
2026-03-04 09:39:07 -0500

    Update sticker.js

--- commands/sticker.js
+++ commands/sticker.js
@@ -36,8 +36,8 @@ async function stickerCommand(sock, chatId, message) {
                 forwardingScore: 999,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '𝗒𝗈𝗎𝗋 newsletter',
-                    newsletterName: '𝗒𝗈𝗎𝗋 𝖻𝗈𝗍 𝗇𝖺𝗆𝖾',
+                    newsletterJid: '120363407561123100@newsletter',
+                    newsletterName: 'LASER-MD',
                     serverMessageId: -1
                 }
             }
@@ -58,8 +58,8 @@ async function stickerCommand(sock, chatId, message) {
                     forwardingScore: 999,
                     isForwarded: true,
                     forwardedNewsletterMessageInfo: {
-                        newsletterJid: '𝗒𝗈𝗎𝗋 newsletter',
-                        newsletterName: '𝗒𝗈𝗎𝗋 𝖻𝗈𝗍 𝗇𝖺𝗆𝖾',
+                        newsletterJid: '120363407561123100@newsletter',
+                        newsletterName: 'LASER-MD',
                         serverMessageId: -1
                     }
                 }
@@ -216,8 +216,8 @@ async function stickerCommand(sock, chatId, message) {
                 forwardingScore: 999,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '𝗒𝗈𝗎𝗋 𝗇𝖾𝗐𝗌𝗅𝖾𝗋',
-                    newsletterName: '𝗒𝗈𝗎𝗋 𝖻𝗈𝗍 𝗇𝖺𝗆𝖾',
+                    newsletterJid: '120363407561123100@newsletter',
+                    newsletterName: 'LASER-MD',
                     serverMessageId: -1
                 }
             }
