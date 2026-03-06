34e95277426d2afe935888f82ed7de0b843da94a
TECHGOAT333 <techgoat681@gmail.com>
2026-03-06 15:40:23 -0500

    Update main.js

--- main.js
+++ main.js
@@ -434,7 +434,6 @@ const { anticallCommand, readState: readAnticallState } = require('./commands/an
 const { pmblockerCommand, readState: readPmBlockerState } = require('./commands/pmblocker');
 const settingsCommand = require('./commands/settings');
 const soraCommand = require('./commands/sora');
-const pinterestCommand = require('./commands/pinterest');
 
 // Global settings
 global.packname = settings.packname;
